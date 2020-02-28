433b9fd0ef659b4cae4143bc2634633d131d58d5
Mustan Pathetic <patheticmustan@gmail.com>
2020-02-27 23:29:58 -0500

    Update UserInfo.java

--- UserInfo.java
+++ UserInfo.java
@@ -1,3 +1,26 @@
+/*
+This project will be graded on:
+1. Pseudocode (4 points): Your pseudocode must accurately describe the steps of the program.
+2. Documentation and Readability (5 points): Your code must be fully commented and employ
+standard Java-style conventions.
+3. User-Friendliness / User-Input (8 points): GUI present and prompts are easy to understand.
+4. Encapsulation (8 points): A standalone class is present with private instance variables, at least one
+constructor, and relevant accessor and mutator methods.
+5. Driver Class (2 points): A driver class is present.
+6. User-Controlled Exit (2 points): The user is asked if they want to continue or exit the program.
+7. Project Requirements (19 points):
+a. Ask user for registration info (2 points)
+b. Remove leading/trailing whitespace (1 point)
+c. Convert all letters to uppercase (1 point)
+d. Complete required character substitutions (1 point)
+e. Swap the first half of the string with the second (6 points; 3 each for even- and odd-length
+strings)
+f. Swap the first 2 characters of the string with the last 2 (2 points)
+g. Swap the middle characters (6 points; 3 each for even- and odd-length strings)
+
+TOTAL: 48 points
+*/
+
 import java.lang.Math;
 
 public class UserInfo {
