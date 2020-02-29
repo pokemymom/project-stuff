ca85729c6af08c8ae3919bc92abfa671cc012b25
PatheticMustan <patheticmustan@gmail.com>
2020-02-28 23:07:41 -0500

    Revert "format"
    This reverts commit 7b012a914dc3e6c86b07f2f51b74bef35e6c6856.

--- UserInfoDriver.java
+++ UserInfoDriver.java
@@ -24,25 +24,25 @@ TOTAL: 48 points
 import javax.swing.*;
 
 public class UserInfoDriver {
-    public static void main(String[] args) {
-        // Test methods
-        JFrame f = new JFrame();
-        UserInfo user = new UserInfo();
-
-        do {
-            String name = JOptionPane.showInputDialog(f, "Enter a new name");
-            user.setName(name);
-
-            String address = JOptionPane.showInputDialog(f, "Enter a new address");
-            user.setAddress(address);
-
-            String username = JOptionPane.showInputDialog(f, "Account Created!\nEnter a new username");
-            user.setUsername(username);
-
-            String password = user.encrypt(JOptionPane.showInputDialog(f, "Enter a new password"));
-            user.setPassword(password);
-
-            JOptionPane.showMessageDialog(f, user.getName() + "\n" + user.getAddress() + "\n" + user.getUsername() + "\n" + user.getPassword());
-        } while (JOptionPane.showConfirmDialog(f, "do you want to repeat?", "exit", JOptionPane.YES_NO_OPTION) == 0);
-    }
+  public static void main(String[] args) {
+    // Test methods
+    JFrame f = new JFrame();
+    UserInfo user = new UserInfo();
+    
+    do {
+      String name = JOptionPane.showInputDialog(f, "Enter a new name");
+      user.setName(name);
+      
+      String address = JOptionPane.showInputDialog(f, "Enter a new address");
+      user.setAddress(address);
+      
+      String username = JOptionPane.showInputDialog(f, "Account Created!\nEnter a new username");
+      user.setUsername(username);
+      
+      String password = user.encrypt(JOptionPane.showInputDialog(f, "Enter a new password"));
+      user.setPassword(password);
+      
+      JOptionPane.showMessageDialog(f, user.getName() + "\n" + user.getAddress() + "\n" + user.getUsername() + "\n" + user.getPassword());
+    } while (JOptionPane.showConfirmDialog(f, "do you want to repeat?", "exit", JOptionPane.YES_NO_OPTION) == 0);
+  }
 }
