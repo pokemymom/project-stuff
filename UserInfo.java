72050b3ecb3d939735eb2006deed6583a9955fad
pokemymom <59845636+pokemymom@users.noreply.github.com>
2020-02-28 22:27:44 -0500

    Update UserInfo.java

--- UserInfo.java
+++ UserInfo.java
@@ -21,111 +21,163 @@ g. Swap the middle characters (6 points; 3 each for even- and odd-length strings
 TOTAL: 48 points
 */
 
-import java.lang.Math;
+public class UserInfo
+{
+// strings, ints, and chars; evenOdd sees if there is an even or odd amount of characters in the string or not
+private String name, address, user, encrypt, str1 = "", str2 = "", str3 = "";
+private int evenOdd = 0, length, halfLength, halfLength2;
+private Character C;
+public UserInfo()
+{
+name = "";
+address = "";
+user = "";
+}
+//gets and sets name
+public void setName(String Name)
+{
+name = Name;
+}
+public String getName()
+{
+return name;
+}
+// gets and sets address
+public void setAddress(String adr)
+{
+address = adr;
+}
+public String getAddress()
+{
+return address;
+}
+// gets and sets username
+public void setUN(String username)
+{
+user = username;
+}
 
-public class Userinfo {
-  //declare
-  private String name;
-  private String address;
-  private String username;
-  private String password;
-  
-  //constructor
-  public UserInfo() {
-    //initialize
-    String name="";
-    String address="";
-    String username="";
-    String password="";
-  }  
-  
-  //combine all the methods
-  public String encrypt(String s){
-    s = removeWhitespaceAndConvertToUpper(s);
-    s = substitute(s);
-    s = swapHalfsForEncrypt(s);
-    s = swapFirst2WithLast2(s);
-    s = swapMiddleChars(s);
-    
-    return s;
-  }  
-  
-  public String removeWhitespaceAndConvertToUpper(String s) {
-    //remove all whitespaces from the password
-    s = s.trim();
-    s = s.toUpperCase();
-    
-    return s;
-  }
-  
-  public String substitute(String s) {
-    s = s.replace ("A", "@");
-    s = s.replace ("E", "=");
-    s = s.replace ("I", "!");
-    s = s.replace ("J", "?");
-    s = s.replace ("O", "*");
-    s = s.replace ("P", "#");
-    s = s.replace ("R", "&");
-    s = s.replace ("S", "$");
-    s = s.replace ("T", "+");
-    s = s.replace ("V", "^");
-    s = s.replace ("X", "%");
-    s = s.replace (" ", "_");
-    
-    return s;
-  }
-  
-  public String swapHalfsForEncrypt(String s) {
-    int count=0;
-    for (int i=0; i<s.length(); i++) {
-      if (Character.isDigit(s.charAt(i))) {
-        count++;  
-        // Even number
-        if(count % 2 == 0){
-          s.endIndex = count/2-1;
-          s_sub = s.substring(0,s.endIndex);
-          last_sub = s.substring(s.endIndex+1);
-          new_str = last_sub + s_sub;
-        } else {
-          s.endIndex = count/2;
-          s_sub = s.substring(0, s.endIndex);
-          last_sub = s.substring(s.endIndex+1);
-          new_str = last_sub + s_sub;
-        }
-      }
-    }
-    
-    return new_str;
-  }
-  
-  // swapFirst2WithLast2
-  public String swapFirst2WithLast2(String s) {
-    first_sub = new_str.substring(0,2);
-    middle_sub = new_str.substring(2,count-2);
-    last_sub = new_str.substring(count-2, count);        
-    new_str = last_sub + middle_sub + first_sub;
-    
-    return new_str;
-  }
-  
-  //swapMiddleChars
-  public String swapMiddleChars(String s) {
-    // even number
-    if(count % 2 == 0){
-      first_str = s.substring(count/2-2,count/2);
-      second_str = s.substring(count/2,count/2+2);
-      first_remain = s.substring(0,count/2-2);
-      second_remain = s.substring(count/2+2, count);
-      new_str = first_remain + second_str + first_str + second_remain;
-    }
-    // odd number
-    if(count % 2 != 0){
-      first_str = s.substring(count/2+1-2,count/2+1);
-      second_str = s.substring(count/2+1,count/2+3);
-      first_remain = s.substring(0,count/2+1-2);
-      second_remain = s.substring(count/2+3, count);
-      new_str = first_remain + second_str + first_str + second_remain;
-    }
-    return new_str;
-  }
+public String getUN()
+{
+return user;
+}
+//returns the encrypted string
+public String encrypt(String s)
+{
+// resets the variables if the user restarts the program
+str1 = ""; str2 = ""; str3 = "";
+if (s.length() < 4){
+s = "4more";
+encrypt = s;}
+else{
+//sees if the string has an even or odd amount of characters and if it has odd, then changes evenOdd to 1
+length = s.length();
+if (length % 2 != 0){
+halfLength = ((length - 1) / 2 + 1);
+evenOdd = 1;}
+else
+halfLength = length / 2;
+s =
+swapMiddleChars(swapFirst2WithLast2(swapHalfsForEncrypt(substitute(removeWhitespaceAndConvertToUpper(s)))));
+encrypt = s;}
+return s;
+}
+//removes the white space and changes string to uppercase
+public String removeWhitespaceAndConvertToUpper (String s)
+{
+// removes the whitespace
+s = s.trim();
+//changes all string to uppercase
+s = s.toUpperCase();
+return s;
+}
+//substitutes specific string with differrent symbols
+public String substitute(String s)
+{
+s = s.replace("A", "@");
+s = s.replace("E", "=");
+s = s.replace("I", "!");
+s = s.replace("J", "?");
+s = s.replace("O", "*");
+s = s.replace("P", "#");
+s = s.replace("R", "&");
+s = s.replace("S", "$");
+s = s.replace("T", "+");
+s = s.replace("V", "^");
+s = s.replace("X", "%");
+s = s.replace(" ", "_");
+return s;
+}
+//swap the last half with the front
+
+public String swapHalfsForEncrypt(String s)
+{
+// stores the first half  of the string
+for (int count = 0; count < halfLength; count++){
+C = s.charAt(count);
+str1 = str1 + C.toString();}
+// stores the second half of the string
+for (int count = halfLength; count < length; count++){
+C = s.charAt(count);
+str2 = str2 + C.toString();}
+// drags the second half in front of the first half
+s = str2 + str1;
+return s;
+}
+// swaps the last 2 with the first 2
+
+public String swapFirst2WithLast2(String s)
+{
+// makes the 2 vairables back to "" instead of last string
+str1 = "";
+str2 = "";
+//gets the first 2 characters and turns it to a strImg
+for (int count = 0; count < 2; count++){
+C = s.charAt(count);
+str1 = str1 + C.toString();}
+// gets the last 2 characters and turns it into a string
+for (int count = length - 2; count < length; count++){
+C = s.charAt(count);
+str2 = str2 + C.toString();}
+// gets everything except the 1st and last 2 letters
+for (int count = 2; count < length - 2; count++){
+C = s.charAt(count);
+str3 = str3 + C.toString();}
+//puts the string in the correct order
+s = str2 + str3 + str1;
+return s;
+}
+// takes the center 4 characters and swaps the first half of those 4 to the last half
+public String swapMiddleChars(String s)
+{
+// resets the variable
+str3 = "";
+// gets the first half and subtract it by 2 and gets it from characters to string
+for (int counter = 0; counter < halfLength - 2; counter++){
+C = s.charAt(counter);
+str3 = str3 + C.toString();}
+
+// gets the characters for the rest of the first half and turns to string and adds on to str3
+for (int counter = halfLength; counter < halfLength + 2; counter++){
+C = s.charAt(counter);
+str3 = str3 + C.toString();}
+// gets the next two characters and turns it to string and adds onto str3
+for (int counter = halfLength - 2; counter < halfLength; counter++){
+C = s.charAt(counter);
+str3 = str3 + C.toString();}
+// gets the rest of characters and turns it into string and adds it onto str3
+for (int counter = halfLength + 2; counter < length; counter++){
+C = s.charAt(counter);
+str3 = str3 + C.toString();}
+return str3;
+}
+public String getEncrypt()
+{
+return encrypt;
+}
+//turns all the info to string and then returns the info
+public String toString(){
+return "Name: " + name + "\nAddress: " + address + "\nUsername: " + user + "\nEncrypted";
+Password:" + encrypt + “\n”";
+}
 }
